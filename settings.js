a39d37fac37e01458e7013b040c730dd6b8781e8
amosyegon075-coder <amosyegon075@gmail.com>
2026-02-14 04:38:40 +0300

    Update bot settings with new name and owner details

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "KAY B",
+  botOwner: Bot", // Your name
+  ownerNumber: '254115158245', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
